84822ab2c09806f3f5157ad659b9251b13a6f295
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2020-12-07 17:11:15 -0800

    wasm parser as option

--- bin/vcd2json.js
+++ bin/vcd2json.js
@@ -8,6 +8,7 @@ const vcd2obj = require('../lib/vcd2obj.js');
 const argv = yargs
   .option('input', {describe: 'path to the input VCD file', alias: 'i'})
   .option('d', {describe: 'dump only defines', alias: 'defs', type: 'count'})
+  .option('w', {describe: 'use WASM parser', alias: 'wasm', type: 'boolean'})
   .demandOption(['input'])
   .help()
   .argv;
